b4ed0888203fa9a2b9a61ae952e87f1578818a46
unknown <giapha@GiaoT470s.neomore.local>
2018-08-23 15:27:24 +0300

    first commit

---
+++ src/com/javatpoint/demo/persistence/Employee.java
@@ -0,0 +1,25 @@
+package com.javatpoint.demo.persistence;
+
+public class Employee {
+	private int id;  
+	private String firstName,lastName;  
+	  
+	public int getId() {  
+	    return id;  
+	}  
+	public void setId(int id) {  
+	    this.id = id;  
+	}  
+	public String getFirstName() {  
+	    return firstName;  
+	}  
+	public void setFirstName(String firstName) {  
+	    this.firstName = firstName;  
+	}  
+	public String getLastName() {  
+	    return lastName;  
+	}  
+	public void setLastName(String lastName) {  
+	    this.lastName = lastName;  
+	}  
+}
